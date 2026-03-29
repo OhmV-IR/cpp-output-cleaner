0fe3309addcfa33aa9ecf68eb5590f4f54f9d8b4
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-03-29 12:15:29 -0400

    fix linux script

--- vars/cleanCPPBuildDir.groovy
+++ vars/cleanCPPBuildDir.groovy
@@ -6,10 +6,27 @@ def call(String outputFolderPath, String dest = "package", boolean isDebug = fal
             rm -rf "${dest}"
             mkdir -p "${dest}"
             cp -r "${outputFolderPath}/." "${dest}"
-            find "${dest}" -name "*.h" -o -name "*.cc" -o -name "*.cmake" -o -name "*.log" -o -name "build.ninja" -o -name "compile_commands.json" -o -name "CMakeCache.txt" -delete
-            find "${dest}" -type d -name "CMakeFiles" -exec rm -rf {} +
-            find "${dest}" -type d -name ".cmake" -exec rm -rf {} +
-            find "${dest}" -type d -name "vcpkg_installed" -exec rm -rf {} +
+	    rm ${dest}/*.cc
+            rm ${dest}/*.pb.h
+	    rm ${dest}/auth.json
+	    rm ${dest}/.ninja_deps
+	    rm ${dest}/.ninja_log
+	    rm ${dest}/build.ninja
+	    rm -rf ${dest}/CMakeFiles
+            rm ${dest}/*.log
+            rm -rf ${dest}/vcpkg_installed
+            rm ${dest}/compile_commands.json
+ 	    rm ${dest}/CMakeCache.txt
+            (
+		cd ${dest}
+		find . -type d -name CMakeFiles -print0 |
+		while IFS= read -r -d '' dir; do
+			parent=\"\$(dirname \"$dir\")\"
+			rm -rf $parent/CMakeFiles
+			rm $parent/*.cmake
+			rm $parent/*.json
+		done
+	    )
         """
     } else {
         pwsh """
@@ -20,13 +37,14 @@ def call(String outputFolderPath, String dest = "package", boolean isDebug = fal
             New-Item -ItemType Directory -Force -Path \$destPath | Out-Null
             Copy-Item -Recurse -Force "${outputFolderPath}/*" "\$destPath/"
 
-            \$targets = @("*.h", "*.cc", "*.cmake", "*.log", "auth.json", "build.ninja", "*.ilk", "compile_commands.json", "CMakeCache.txt")
+            \$targets = @("*.h", "*.cc", "*.cmake", "*.log", "auth.json", "build.ninja", "*.ilk", "compile_commands.json", "CMakeCache.txt", ".ninja_deps", ".ninja_log")
             if (-not \$isDebug) { \$targets += "*.pdb" }
 
             Get-ChildItem -Path \$destPath -Include \$targets -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
             Get-ChildItem -Path \$destPath -Directory -Filter "CMakeFiles" -Recurse | Remove-Item -Recurse -Force
             Get-ChildItem -Path \$destPath -Directory -Filter ".cmake" -Recurse | Remove-Item -Recurse -Force
             Get-ChildItem -Path \$destPath -Directory -Filter "vcpkg_installed" -Recurse | Remove-Item -Recurse -Force
+	    Get-ChildItem -Path \$destPath\\tests -File -Filter "cmake_test_discovery*.json" | Remove-Item
         """
     }
 }
